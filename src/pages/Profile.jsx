333773d46ed417adf34ee904f5e692884d928357
Varun <varunrawat343@gmail.com>
2024-06-09 01:54:34 +0530

    Fix profile issue

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,34 @@
-import React, { useContext, useEffect } from "react";
-import { Context } from "../main";
+import React, { useContext, useEffect, useState } from "react";
+import { Context, server } from "../main";
 import Loader from "../components/Loader";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const Profile = () => {
-  const { isAuthenticated, loading, user } = useContext(Context);
+  const { isAuthenticated, loading } = useContext(Context);
+  const [myUser, setMyUser] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${server}/users/me`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setMyUser(res.data.user);
+      })
+      .catch((error) => {
+        setMyUser("");
+      });
+  });
 
   if (!isAuthenticated) return <Navigate to={"/login"} />;
+
   return loading ? (
     <Loader />
   ) : (
     <div>
-      <h1>{user.name}</h1>
-      <p>{user.email}</p>
+      <h1>{myUser.name}</h1>
+      <p>{myUser.email}</p>
     </div>
   );
 };
